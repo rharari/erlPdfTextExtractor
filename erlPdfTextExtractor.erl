91aa476e29bc6414056174b8a8e5e0b0dbffb5af
Ricardo Harari <ricardo.harari@gmail.com>
2015-02-26 02:55:34 -0300

    Update erlPdfTextExtractor.erl

--- erlPdfTextExtractor.erl
+++ erlPdfTextExtractor.erl
@@ -1,30 +1,87 @@
 %% @author Ricardo A. Harari - ricardo.harari@gmail.com
 %% @doc pdf2txt/2 - extract txt from a pdf file and save it to disk
 %% @doc ex: erlPdfTextExtractor:pdf2txt("/inbox/file1.pdf", "/outbox/file1.txt")
-%% TODO: fix some bugs 
+%% TODO: finish this
 -module(erlPdfTextExtractor).
 
 %% ====================================================================
-%% API functions
-%% ====================================================================
--export([pdf2txt/2, extract/1]).
+-export([pdf2txt/2]).
 
 pdf2txt(PdfFile, TxtOutFile) ->
-	case extract(PdfFile) of
-		{error, Why} -> {error, Why};
-		Txt -> file:write_file(TxtOutFile, Txt)
-	end.
-
-extract(PdfFile) ->
 	case file:read_file(PdfFile) of
 		{error, Why} -> {error, Why};
-		{ok, Content} -> 
-			stripStream(Content)
-	end.	
+		{ok, Content} -> file:write_file(TxtOutFile, process(Content))
+	end.
 
 %% ====================================================================
-%% Internal functions
-%% ====================================================================
+cut([], _, _) -> {[], 0};
+cut(Content, S1, S2) ->
+	L1 = byte_size(S1), L2 = byte_size(S2),
+	case binary:match(Content, S1) of
+		{I1, L1} -> 
+			case binary:match(Content, S2) of
+				{I2, L2} -> {binary:part(Content, I1 + L1, I2 - I1 - L1), I2 + L2};
+				_Else -> {[], 0}
+			end;
+		_Else -> {[], 0}
+	end.
+
+process(Content) ->
+	Lst = getLstObj(Content),
+	processObj(Lst).
+
+getLstObj(Content) ->
+	case cut(Content, <<"obj">>, <<"endobj">>) of
+		{_, 0} -> [];
+		{Z, N} -> [Z] ++ getLstObj(binary:part(Content, N, byte_size(Content) - N))
+	end.
+
+processObj([]) -> [];
+processObj([H|T]) ->
+	Option = getObjOption(H),
+	case shouldProcess(Option) of
+		false -> processObj(T);
+		true -> [decodeObj(H, Option)] ++ processObj(T)
+	end.
+
+getObjOption(O) ->
+	case cut(O, <<"<<">>, <<">>">>) of
+		{_, 0} -> [];
+		{Z, _} -> Z
+	end.
+
+decodeObj(Obj, Option) ->
+	case shouldProcess(Option) of
+		false -> [];
+		true -> retrieveStream(Obj, Option)
+	end.
+
+matchOption([], _) ->
+	false;
+
+matchOption(Option, What) ->
+	Check = fun(X) when X =:= nomatch -> false; (_) -> true end,
+	Check(binary:match(Option, What)).
+
+shouldProcess(Option) ->	
+	not (matchOption(Option, <<"Length1">>) andalso matchOption(Option, <<"Type">>) andalso matchOption(Option, <<"Subtype">>)).
+
+retrieveStream(Obj, Option) ->
+	case matchOption(Option, <<"Length">>) of 
+		false -> stripStream(Obj);
+		true ->
+			Z = uncompress(stripStream(Obj)),
+			Data = cutBTET(Z),
+			case Data of 
+				[] -> transform(Z);
+				_ -> stripChar(Data)
+			end
+	end.
+
+transform(Z) ->
+	% TODO transform beginbfchar and beginbfrange   
+	[].
+
 stripStream(Content) ->
 	case binary:match(Content, <<"stream">>) of
 		{I0tmp, 6} ->
@@ -32,20 +89,16 @@ stripStream(Content) ->
 			case binary:match(Content, <<"endstream">>) of
 				{I1tmp, 9} ->
 					I1 = I1tmp - skipChar(binary:part(Content, I1tmp - 2, 1)) - skipChar(binary:part(Content, I1tmp - 1, 1)),
-					% Data = stripChar(stripBTET(uncompress(binary:part(Content, I0, I1-I0)))),
-					Data = stripChar(uncompress(binary:part(Content, I0, I1-I0))),
-					[Data] ++ stripStream( binary:part(Content, I1tmp + 9, byte_size(Content) - I1tmp - 9));
+					binary:part(Content, I0, I1-I0);
 				_Else -> []
 			end;
 		_Else -> []
 	end.
 
- % TODO: is not considering numbers and all text
-stripChar([]) -> [];
 stripChar(Z) ->
 	case binary:match(Z, <<"(">>) of
 		{I0, 1} ->
-				Z1 = binary:part(Z, I0 + 2, 1), % TODO: may raise exception if I0+2 greather than Z length
+				Z1 = binary:part(Z, I0 + 2, 1),
 				case Z1 of 
 					<<")">> -> [binary:part(Z, I0 + 1, 1)] ++ stripChar(binary:part(Z, I0 + 3, byte_size(Z) - I0 - 3));
 					_Else -> []
@@ -53,24 +106,27 @@ stripChar(Z) ->
 		_Else -> []
 	end.
 
-
 cutBTET([]) -> [];
-cutBTET(Z) ->
-	case binary:match(Z, <<"BT">>) of
-		{I0, 2} ->
-			case binary:match(Z, <<"ET">>) of
-				{I1, 2} -> binary:part(Z, I0 + 2, I1 - I0 - 2);
-				_Else -> []
-			end;
-		_Else -> []
-	end.
-
+cutBTET(Z) -> A1 = [X || {X, 2} <- binary:matches(Z, [<<"BT">>, <<"ET">>])], cutBTET2(Z, A1).
+cutBTET2(Z, [H1,H2|T]) -> iolist_to_binary([binary:part(Z, H1 + 2, H2 - H1 - 2)] ++ [cutBTET2(Z, T)] );
+cutBTET2(_, _) -> [].
 
 uncompress(Z) ->
 	try zlib:uncompress(Z) of Y -> Y
 	catch error:data_error -> [] % ie: DCTDecode indicates a JPEG, etc... - discard it
 	end.
 
-skipChar(<<14>>) -> 1; % /r
-skipChar(<<10>>) -> 1; % /n
+skipChar(Z) when Z =:= <<14>> orelse Z =:= <<10>> -> 1; % /r or /n
 skipChar(_) -> 0.
+
+% deprecated
+%zcutBTET([]) -> [];
+%zcutBTET(Z) ->
+%	case binary:match(Z, <<"BT">>) of
+%		{I0, 2} ->
+%			case binary:match(Z, <<"ET">>) of
+%				{I1, 2} -> iolist_to_binary([binary:part(Z, I0 + 2, I1 - I0 - 2)] ++ cutBTET(binary:part(Z, I1 + 2, byte_size(Z) - I1 - 2)));
+%				_Else -> []
+%			end;
+%		_Else -> []
+%	end.
